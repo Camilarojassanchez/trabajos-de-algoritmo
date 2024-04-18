beb421c8ea47470ee815cc7ea165f864f8e50d72
Marlongonzalezmolano <molanomarlon55g@gmail.com>
2024-04-18 10:28:11 -0500

    ejercicio1

---
+++ ejercicio1.pycamilarojas.py
@@ -0,0 +1,24 @@
+a=2
+b=5
+suma=a+b
+print("la suma es:",suma)
+
+a=9
+b=4
+resta=a-b
+print("la resta es:",resta)
+
+a=2
+b=5
+multiplicacion=a*b
+print("la multiplicacion es:",multiplicacion)
+
+a=6
+b=3
+division=a/b
+print("la division es:",division)
+
+a=19
+b=4
+divisionresiduo=a%b
+print("la division residuo:",divisionresiduo)
